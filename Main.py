e9a5663843d80b8288a79140c67382e591f0b239
aranyketheeswaran <arany.ketheeswaran@nmbu.no>
2021-12-26 21:14:19 +0100

    add sequence diagram

--- Main.py
+++ Main.py
@@ -1,6 +1,11 @@
 from Ansatt import Ansatt
+from Systemet import Systemet
 
 arany = Ansatt("arany98", "Cutie")
+system = Systemet("mosti", 10)
+navn = input("Navn: ")
+år = input("År: ")
+system = Systemet(navn, år)
 
 
 arany.logg_inn_bekreftelse()
--- Systemet.py
+++ Systemet.py
@@ -1,1 +1,8 @@
 class Systemet:
+
+    def __init__(self, navn, år):
+        self.navn = navn
+        self.år = år
+        self.mail = f"{navn}.{år}@hotmail.com"
+
+    def registrer_bruker(self, navn, passord):
